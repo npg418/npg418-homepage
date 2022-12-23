4764dc5e9463e2e7fff56acd0b30038bebd4fec0
NPG418 <narupenguin919@gmail.com>
2022-12-23 20:54:31 +0900

    delayとdurationの設定

--- islands/FadeUpSection.tsx
+++ islands/FadeUpSection.tsx
@@ -1,18 +1,33 @@
-import { JSX } from 'preact';
 import FadeUp from '@/islands/FadeUp.tsx';
-import { Replace } from '@/types/util.ts';
 import { Child } from '@/types/component.ts';
+import { JSX } from 'preact/jsx-runtime';
 
 export default function FadeUpSection(
-    props: Replace<JSX.HTMLAttributes<HTMLElement>, {
+    props: {
         children: Child[] | Child;
-    }>,
+        delay?: number;
+        duration?: number;
+        as?: JSX.ElementType;
+        props?: (typeof props.as) extends undefined ? never
+            : JSX.HTMLAttributes<HTMLElement>;
+    },
 ) {
-    return (
-        <>
-            {props.children instanceof Array
-                ? props.children.map((child) => <FadeUp>{child}</FadeUp>)
-                : <FadeUp>{props.children}</FadeUp>}
-        </>
-    );
+    const children = props.children instanceof Array
+        ? props.children.map((child, i) => (
+            <FadeUp
+                duration={props.duration}
+                delay={props.delay && props.delay * (i + 1)}
+            >
+                {child}
+            </FadeUp>
+        ))
+        : (
+            <FadeUp duration={props.duration} delay={props.delay}>
+                {props.children}
+            </FadeUp>
+        );
+
+    return props.as
+        ? <props.as {...props.props}>{children}</props.as>
+        : <>{children}</>;
 }
