3b41af939e2e667fbf633d5e80519d859601f704
NPG418 <narupenguin919@gmail.com>
2022-12-27 13:54:09 +0900

    fmt

--- routes/index.tsx
+++ routes/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
             <div class='flex items-center'>
                 <img class='w-1/2' src='npg418.png' />
                 <h1 class='font-bold text-7xl ml-4 flex flex-col'>
-                       <span class='text-green-500'>Hello!</span>
+                    <span class='text-green-500'>Hello!</span>
                     <span>I'm NPG418</span>
                 </h1>
             </div>
