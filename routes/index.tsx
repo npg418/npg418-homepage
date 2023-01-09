adc3738f582df1c8ebfefc2adc506963ea2bbe98
NPG418 <narupenguin919@gmail.com>
2023-01-09 21:41:02 +0900

    preloadタグの最適化

--- routes/index.tsx
+++ routes/index.tsx
@@ -7,7 +7,12 @@ export default function Home() {
         <>
             <Head>
                 <title>NPG418 Homepage</title>
-                <link rel='preload' href='npg418.png' as='image/png' />
+                <link
+                    rel='preload'
+                    href='npg418.png'
+                    as='image'
+                    type='image/png'
+                />
             </Head>
             <div class='flex items-center mb-32'>
                 <img class='w-1/2' src='npg418.png' alt='NPG418' />
