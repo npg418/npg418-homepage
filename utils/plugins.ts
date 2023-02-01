54b135fa8896d8bd4b46a19603a172121c6ebb17
NPG418 <narupenguin919@gmail.com>
2023-02-01 10:42:35 +0900

    fmt

--- utils/plugins.ts
+++ utils/plugins.ts
@@ -3,7 +3,7 @@ import { renderToString } from 'preact-render-to-string';
 import MarkdownIt from 'markdown-it';
 import { FaRegCopy } from 'react-icons/fa';
 import { RenderRule } from 'markdown-it/renderer.d.ts';
-import { dedent } from "@/utils/strings.ts";
+import { dedent } from '@/utils/strings.ts';
 
 export const anchor: MarkdownIt.PluginSimple = (md) => {
     md.core.ruler.push('anchor', ({ tokens }) => {
@@ -38,11 +38,14 @@ export const codeblock: MarkdownIt.PluginSimple = (md) => {
         }
 
         return dedent`
-            <pre class="${md.options.langPrefix + (lang || 'none')} ${tx`rounded relative group`}">
-                ${filename
+            <pre class="${
+            md.options.langPrefix + (lang || 'none')
+        } ${tx`rounded relative group`}">
+                ${
+            filename
                 ? `<div class="${tx`py-1 px-2 bg-gray(300 dark:600) w-[fit-content] -mt-4 -ml-4 mb-1 rounded-br`}">${filename}</div>`
                 : ''
-            }
+        }
                 <code>${target.content}</code>
                 <button class="${tx`absolute top-4 right-4 opacity-0 transition-opacity group-hover:opacity-100`}" title="クリップボードにコピー" onclick="copyCode(this)">
                     ${renderToString(FaRegCopy({}))}
