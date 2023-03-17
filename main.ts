05ba9aec7860f006f1f92b9d09fda8cf9824452d
NPG418 <narupenguin919@gmail.com>
2023-03-17 20:08:23 +0900

    fmt

--- main.ts
+++ main.ts
@@ -5,9 +5,16 @@
 /// <reference lib="deno.ns" />
 
 import { start } from '$fresh/server.ts';
-import manifest from './fresh.gen.ts';
+import manifest from "@/fresh.gen.ts";
 
 import twindPlugin from '$fresh/plugins/twind.ts';
-import twindConfig from './twind.config.ts';
+import twindConfig from "@/twind.config.ts";
 
-await start(manifest, { plugins: [twindPlugin(twindConfig)] });
+await start(manifest, {
+    plugins: [twindPlugin(twindConfig)],
+    render(ctx, render) {
+        ctx.lang = 'ja';
+        render();
+    },
+    port: 3000,
+});
