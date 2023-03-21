a96260ab5a62b3f921af6c93783473ea4ccd191d
NPG418 <narupenguin919@gmail.com>
2023-03-21 16:55:33 +0900

    faviconの追加

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,10 +1,14 @@
 import { AppProps } from '$fresh/server.ts';
 import Header from '@/components/Header.tsx';
 import ColormodeClient from '@/islands/ColormodeClient.tsx';
+import { Head } from '$fresh/runtime.ts';
 
 export default function App({ Component }: AppProps) {
     return (
         <html>
+            <Head>
+                <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+            </Head>
             <ColormodeClient />
             <body class='min-h-screen'>
                 <Header />
