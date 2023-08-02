b32ffed9779ffcad493624ea0e416ca0047cf873
NPG418 <narupenguin919@gmail.com>
2023-08-02 14:23:52 +0900

    footer, heroの追加

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -1,6 +1,7 @@
 import { asset, Head } from '$fresh/runtime.ts';
 import { AppProps } from '$fresh/server.ts';
 import { theme } from '@/utils/colormode.ts';
+import Footer from '@/components/Footer.tsx';
 
 export default function App({ Component }: AppProps) {
   return (
@@ -9,10 +10,11 @@ export default function App({ Component }: AppProps) {
         <link rel='shortcut icon' href='favicon.ico' type='image/x-icon' />
         {theme.value === 'auto' ? <script defer src={asset('auto-colormode.js')} /> : null}
       </Head>
-      <body class='min-h-screen bg-gray-300 text-gray-900'>
-        <main class='max-w-screen-lg mx-auto'>
+      <body class='min-h-screen bg-gray-300 text-gray-900 flex flex-col'>
+        <main class='max-w-screen-lg mx-auto flex-1'>
           <Component />
         </main>
+        <Footer />
       </body>
     </html>
   );
--- routes/index.tsx
+++ routes/index.tsx
@@ -1,4 +1,5 @@
 import { asset, Head } from '$fresh/runtime.ts';
+import IconArrowCurveLeft from 'tabler-icons/arrow-curve-left.tsx';
 
 export default function IndexPage() {
   return (
@@ -6,8 +7,18 @@ export default function IndexPage() {
       <Head>
         <title>NPG418 HP</title>
       </Head>
-      <h1 class='text-8xl text-center'>NPG418 IS HERE!!</h1>
-      <img src={asset('penguin_transparent.png')} alt='NPG418' class='mx-auto' />
+      <div class='text-center'>
+        <h1 class='text-8xl'>NPG418 IS HERE!!</h1>
+        <figure class='relative w-fit inline-block'>
+          <img src={asset('penguin_transparent.png')} alt='NPG418' />
+          <figcaption class='absolute top-20 right-10 -rotate-12'>
+            <p class='flex items-start text-lg'>
+              <IconArrowCurveLeft class='-rotate-90 translate-y-2' />
+              me
+            </p>
+          </figcaption>
+        </figure>
+      </div>
     </>
   );
 }
