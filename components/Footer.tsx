3f77a255fd507255b87a00c6eeed994533ea3b13
NPG418 <narupenguin919@gmail.com>
2024-03-02 14:42:04 +0900

    copyrightの更新

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer class='text-center mt-16'>
-      <small>Copyright &copy; 2023 NPG418 All Rights Reserved</small>
+      <small>Copyright &copy; 2024 NPG418 All Rights Reserved</small>
     </footer>
   );
 }
