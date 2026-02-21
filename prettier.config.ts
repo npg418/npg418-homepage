289d493bd0798c862a0a66bd5d96bc8421ee9d6d
NPG418 <narupenguin919@gmail.com>
2026-02-21 20:56:06 +0900

    feat(config): Prettierプラグインを追加し、設定を更新

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "prettier";
 
 export default {
-  plugins: ["prettier-plugin-astro"],
+  plugins: ["prettier-plugin-astro", "prettier-plugin-organize-imports", "prettier-plugin-astro-organize-imports"],
   printWidth: 120,
   overrides: [
     {
